e3fc73a718f25f4b199e939cf1015e7b0166042b
ClaytonW40 <clayton_williams40@outlook.com>
2025-11-05 14:52:47 -0600

    added styling to the card

--- components/card.tsx
+++ components/card.tsx
@@ -13,7 +13,7 @@ export default function Card({ imgSrc, text, altText,title }: CardProps) {
         <div className ="fade-in-up">
             <h1>{title}</h1>
             
-            <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 flex gap-6">
+            <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 flex gap-6 ">
                 <img src={imgSrc} alt={altText} className="w-auto h-48 object-cover rounded-md mb-4 " />
                 <p className="text-center text-lg font-medium text-wrap">{text}</p>
             </div>
