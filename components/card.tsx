3b3e6049fde68c06eb26a8bcf685c3d480af0f66
ClaytonW40 <clayton_williams40@outlook.com>
2025-11-06 09:31:36 -0600

    Added styling, removed template images

--- components/card.tsx
+++ components/card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 export default function Card({ imgSrc, text, altText,title }: CardProps) {
     return (
         
-        <div className ="fade-in-up">
+        <div className ="fade-in-up mr-8 ml-8">
             <h1>{title}</h1>
             
             <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 flex gap-6 ">
