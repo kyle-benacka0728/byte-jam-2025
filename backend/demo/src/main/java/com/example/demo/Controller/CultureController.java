1a6057433ddc5e3e1d361ef42971c4c7dd594925
CC3031 <acunafernando617@gmail.com>
2025-11-06 11:18:21 -0600

    fixed routing issue with cultures/cultureId

--- backend/demo/src/main/java/com/example/demo/Controller/CultureController.java
+++ backend/demo/src/main/java/com/example/demo/Controller/CultureController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @CrossOrigin(origins="*")
 @RestController
-@RequestMapping("/cultures")
+@RequestMapping("/cultures/{cultureId}")
 public class CultureController {
     private final CulturesRepository culturesRepository;
 
