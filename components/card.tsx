6ea544ee363ea0a6d9ae0ea1289963e48490be75
ClaytonW40 <clayton_williams40@outlook.com>
2025-11-10 08:11:50 -0600

    Potential fix for facts issue

--- components/card.tsx
+++ components/card.tsx
@@ -1,20 +1,17 @@
+import { CultureCardProps } from '@/types';
 
-import { Culture, CultureCardProps } from '@/types';
-
-export default function Card({ name, url, altText, bullet1, bullet2, bullet3, bullet4}: CultureCardProps) {
-    const bullets = [bullet1, bullet2, bullet3, bullet4].filter(Boolean);
-    return (
-        
-        <div className ="fade-in-up mr-8 ml-8">
-            <h1>{name}</h1>
-            
-            <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 flex gap-6 ">
-                <img src={url} alt={altText} className="w-auto h-48 object-cover rounded-md mb-4 " />
-                <p className="text-center text-lg font-medium text-wrap">{bullets}</p>
-            </div>
-        </div>
-    
-        
-        
-    );
+export default function Card({ name, url, altText, bullets }: CultureCardProps) {
+  return (
+    <div className="fade-in-up mr-8 ml-8">
+      <h1>{name}</h1>
+      <div className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 flex gap-6">
+        <img src={url} alt={altText} className="w-auto h-48 object-cover rounded-md mb-4" />
+        <ul className="text-lg font-medium list-disc list-inside">
+          {bullets.map((b, idx) => (
+            <li key={idx}>{b}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
